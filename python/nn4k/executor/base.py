9c416e9527ab6f6e0f2a0a9c0fe685e5f360c2bf
huaidong.xhd <huaidong.xhd@antgroup.com>
2023-12-11 16:54:18 +0800

    feat: Implement OpenAIInvoker
    (cherry picked from commit 75fe2b11f605f3682fefa9be42f1cbd0dab200cc)

--- python/nn4k/executor/base.py
+++ python/nn4k/executor/base.py
@@ -1,3 +1,14 @@
+# Copyright 2023 Ant Group CO., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied.
+
 from abc import ABC, abstractmethod
 
 
@@ -10,11 +21,7 @@ class NNExecutor(ABC):
     def from_config(cls, nn_config, **kwargs):
         pass
 
-    def __init__(self,
-             backend_model,
-             backend_tokenizer,
-             init_args,
-             **kwargs):
+    def __init__(self, backend_model, backend_tokenizer, init_args, **kwargs):
         self.backend_model = backend_model
         self.backend_tokenizer = backend_tokenizer
         self.init_args = init_args
@@ -22,7 +29,6 @@ class NNExecutor(ABC):
 
 
 class LLMExecutor(NNExecutor):
-
     @classmethod
     def from_config(cls, nn_config: dict, **kwargs):
         """
@@ -45,7 +51,9 @@ class LLMExecutor(NNExecutor):
         """
         The entry point of SFT execution in a certain pod.
         """
-        raise NotImplementedError(f"{self.__class__.__name__} does not support RL-Tuning.")
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not support RL-Tuning."
+        )
 
     def execute_inference(self, args, **kwargs):
         dataset = args.parse_dataset()
@@ -70,5 +78,3 @@ class HfLLMExecutor(NNExecutor):
 class DeepKeExecutor(NNExecutor):
 
     pass
-
-
