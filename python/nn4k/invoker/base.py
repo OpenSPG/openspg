75fe2b11f605f3682fefa9be42f1cbd0dab200cc
huaidong.xhd <huaidong.xhd@antgroup.com>
2023-12-11 16:54:18 +0800

    feat: Implement OpenAIInvoker

--- python/nn4k/invoker/base.py
+++ python/nn4k/invoker/base.py
@@ -1,4 +1,15 @@
 # coding: utf-8
+# Copyright 2023 Ant Group CO., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied.
+
 # Copyright (c) Antfin, Inc. All rights reserved.
 import json
 import os
@@ -24,7 +35,9 @@ class NNInvoker(ABC):
 
     def __init__(self, nn_executor: NNExecutor) -> None:
         if os.getenv("NN4K_DEBUG") is None:
-            raise EnvironmentError("In prod env, only NNInvoker.from_config is allowed for creating an nn_invoker.")
+            raise EnvironmentError(
+                "In prod env, only NNInvoker.from_config is allowed for creating an nn_invoker."
+            )
         super().__init__()
         self._nn_executor: NNExecutor = nn_executor
 
@@ -33,15 +46,15 @@ class LLMInvoker(NNInvoker):
     def __init__(self, nn_executor: LLMExecutor) -> None:
         super().__init__(nn_executor)
 
-    def submit_inference(self, submit_mode='k8s'):
+    def submit_inference(self, submit_mode="k8s"):
         # TODO. maybe like:
         # engine.submit(self._nn_config, "xx_executor.execute_inference()")
         pass
 
-    def submit_sft(self, submit_mode='k8s'):
+    def submit_sft(self, submit_mode="k8s"):
         pass
 
-    def submit_rl_tuning(self, submit_mode='k8s'):
+    def submit_rl_tuning(self, submit_mode="k8s"):
         pass
 
     # def deploy(cls, args, deploy_mode='k8s'):
@@ -86,4 +99,5 @@ class LLMInvoker(NNInvoker):
             return o
         elif nn_config.get("invoker_type", "LLM") == "OpenAI":
             from nn4k.invoker.openai_invoker import OpenAIInvoker
+
             return OpenAIInvoker.from_config(nn_config)
