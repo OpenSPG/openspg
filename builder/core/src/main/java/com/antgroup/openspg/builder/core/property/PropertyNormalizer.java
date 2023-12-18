1435c85cf4fc6b7a566965ce36ddd0c0b5e656d7
baifuyu <fuyu.bfy@antgroup.com>
2023-12-18 16:06:49 +0800

    bugfix

--- builder/core/src/main/java/com/antgroup/openspg/builder/core/property/PropertyNormalizer.java
+++ builder/core/src/main/java/com/antgroup/openspg/builder/core/property/PropertyNormalizer.java
@@ -5,18 +5,11 @@ import com.antgroup.openspg.builder.model.exception.BuilderException;
 import com.antgroup.openspg.builder.model.exception.PropertyNormalizeException;
 import com.antgroup.openspg.builder.model.record.property.BasePropertyRecord;
 
-/**
- * 属性标准化，针对以下情况会执行对应的标准化操作：
- *
- * <ul>
- *   <li>1. 当属性是基础类型时，则对属性进行类型校验及转化到正确类型
- *   <li>2. 当属性是非基础类型时，则对属性进行属性挂载
- * </ul>
- */
 public interface PropertyNormalizer {
-  /** 初始化属性标准策略 */
+
+  /** Initialize property normalization strategy. */
   void init(BuilderContext context) throws BuilderException;
 
-  /** 输入一条spg属性记录，对该属性进行标准化 */
+  /** Input an SPG property record and normalize the property value. */
   void propertyNormalize(BasePropertyRecord record) throws PropertyNormalizeException;
 }
