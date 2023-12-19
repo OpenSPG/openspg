a806f6c0e21da14a507a2d51074ccf16cdd8cbe4
xionghuaidong <huaidong.xhd@antgroup.com>
2023-12-19 19:43:47 +0800

    feat(nn4k): split nn4k/executor/base.py

--- python/nn4k/executor/deepke.py
+++ python/nn4k/executor/deepke.py
@@ -8,3 +8,10 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied.
+
+from nn4k.executor import NNExecutor
+
+
+class DeepKeExecutor(NNExecutor):
+
+    pass
