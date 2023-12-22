e7551262fbffee0e9f64a1d89080e7042d8df163
xionghuaidong <huaidong.xhd@antgroup.com>
2023-12-22 11:47:00 +0800

    feat(nn4k): implement nn4k packaging

--- python/nn4k/nn4k/__init__.py
+++ python/nn4k/nn4k/__init__.py
@@ -8,3 +8,7 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied.
+
+
+__package_name__ = "openspg-nn4k"
+__version__ = "0.0.1-beta2"
