6b179e454234168d998924eab1bf982cd71318f7
baifuyu <fuyu.bfy@antgroup.com>
2024-01-09 19:50:33 +0800

    feat(release): release 0.0.2-beta2 (#75)

--- dev/release/server/buildx-release-server.sh
+++ dev/release/server/buildx-release-server.sh
@@ -10,6 +10,6 @@
 # or implied.
 
 docker buildx build -f Dockerfile --platform linux/arm64/v8,linux/amd64 --push \
-  -t openspg/openspg-server:0.0.2-beta1 \
+  -t openspg/openspg-server:0.0.2-beta2 \
   -t openspg/openspg-server:latest \
   .
