3839e15e1722256d8f49111ac4ed3cf28893406f
baifuyu <fuyu.bfy@antgroup.com>
2024-03-08 10:20:30 +0800

    add license

--- server/api/http-server/src/main/java/com/antgroup/openspg/server/api/http/server/openapi/ConceptInstanceController.java
+++ server/api/http-server/src/main/java/com/antgroup/openspg/server/api/http/server/openapi/ConceptInstanceController.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2023 OpenSPG Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ */
+
 package com.antgroup.openspg.server.api.http.server.openapi;
 
 import com.antgroup.openspg.server.api.facade.dto.schema.request.ConceptLevelInstanceRequest;
@@ -8,6 +21,8 @@ import com.antgroup.openspg.server.api.http.server.HttpBizCallback;
 import com.antgroup.openspg.server.api.http.server.HttpBizTemplate;
 import com.antgroup.openspg.server.biz.common.util.AssertUtils;
 import com.antgroup.openspg.server.biz.schema.ConceptInstanceManager;
+import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,9 +30,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.Set;
 
 @Controller
 @RequestMapping("/public/v1/conceptInstance")
