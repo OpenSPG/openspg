17d7bd7782525be633f5508d5ebb5f1c37e4ed9c
Donghai <donghai.ydh@antgroup.com>
2024-05-11 14:35:13 +0800

    update

--- reasoner/warehouse/warehouse-common/src/main/java/com/antgroup/openspg/reasoner/warehouse/common/config/GraphTimeCondition.java
+++ reasoner/warehouse/warehouse-common/src/main/java/com/antgroup/openspg/reasoner/warehouse/common/config/GraphTimeCondition.java
@@ -1,40 +1,44 @@
 /*
- * Ant Group
- * Copyright (c) 2004-2024 All Rights Reserved.
+ * Copyright 2023 OpenSPG Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
  */
+
 package com.antgroup.openspg.reasoner.warehouse.common.config;
 
 import com.google.common.base.Splitter;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.Map;
 import lombok.Data;
-import org.apache.commons.lang3.StringUtils;
 
-/**
- * @author donghai.ydh
- * @version GraphTimeCondition.java, v 0.1 2024-05-11 09:50 donghai.ydh
- */
 @Data
 public class GraphTimeCondition implements Serializable {
-  private long startMs;
-  private long endMs;
+  private long startMs = 0;
+  private long endMs = 0;
+  private long intervalMs = 0;
 
   public GraphTimeCondition(String configStr) {
-    List<Long> msList = new ArrayList<>(2);
-    Splitter.on(",")
-        .split(configStr)
-        .forEach(
-            s -> {
-              s = s.trim();
-              if (StringUtils.isEmpty(s) || "null".equals(s.toLowerCase(Locale.ROOT))) {
-                msList.add(null);
-                return;
-              }
-              msList.add(Long.parseLong(s));
-            });
-    this.startMs = msList.get(0);
-    this.endMs = msList.get(1);
+    Map<String, String> map = Splitter.on(",").withKeyValueSeparator(":").split(configStr);
+    for (String key : map.keySet()) {
+      String value = map.get(key);
+      if ("startMs".equals(key)) {
+        this.startMs = Long.parseLong(value);
+      } else if ("endMs".equals(key)) {
+        this.endMs = Long.parseLong(value);
+      } else if ("intervalMs".equals(key)) {
+        this.intervalMs = Long.parseLong(value);
+      }
+    }
+    if (intervalMs > 0) {
+      endMs = System.currentTimeMillis();
+      startMs = endMs - intervalMs;
+    }
   }
 }
