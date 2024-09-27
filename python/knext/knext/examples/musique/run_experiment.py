41a2efaffd268610b28044c2f7a925780eabb2b3
AACEPT <zhanghang21@m.fudan.edu.cn>
2024-09-27 18:55:03 +0800

    Add the expr num to 200

--- python/knext/knext/examples/musique/run_experiment.py
+++ python/knext/knext/examples/musique/run_experiment.py
@@ -275,7 +275,7 @@ def run_deepseek_divide_and_conquer_experiment():
     failure_list = []
     exp_list = []
 
-    expr_num = 100
+    expr_num = 200
 
     for i in range(expr_num):
         exp = False
