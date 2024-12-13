dc72cf7cb10658c9c897d8c5695c2627a27c2884
锦呈 <zhangxinhong.zxh@antgroup.com>
2024-12-13 15:34:24 +0800

    fix

--- python/knext/knext/reasoner/rest/models/reason_markdown_request.py
+++ python/knext/knext/reasoner/rest/models/reason_markdown_request.py
@@ -15,7 +15,7 @@ import re  # noqa: F401
 
 import six
 
-from openapi_client.configuration import Configuration
+from knext.common.rest.configuration import Configuration
 
 
 class ReportMarkdownRequest(object):
